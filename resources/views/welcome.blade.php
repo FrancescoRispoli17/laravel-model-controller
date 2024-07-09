a3d97b3adc73d199740fd7f25429b0523e72e376
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-07-09 02:31:54 +0200

    [UPD] movie's cards in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,20 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    
+    <div class="container mt-5">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="card m-2" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title">{{$movie['title']}}</h3>
+                        <p class="card-text fs-5">{{$movie['nationality']}}</p>
+                        <p class="card-text fs-5">{{$movie['date']}}</p>
+                        <p class="card-text fs-5">{{$movie['vote']}}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </body>
 </html>
